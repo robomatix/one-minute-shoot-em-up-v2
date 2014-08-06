d498aa894d68cd315fa0f3ff7f76b0dab5d47298
robomatix <robomatix@robomatix.net>
2014-08-06 13:17:10 +0200

    Title in the menu state > Code formated

--- game/states/menu.js
+++ game/states/menu.js
@@ -1,34 +1,31 @@
-
 'use strict';
-function Menu() {}
+function Menu() {
+}
 
 Menu.prototype = {
-  preload: function() {
+    preload: function () {
 
-  },
-  create: function() {
+    },
+    create: function () {
 
 
-      // Title
-      //var titleStyle = { font: '36px Arial', fill: '#ffffff', align: 'center'};
-      //this.titleText = this.game.add.text(this.game.world.centerX, 30, 'The One Minute Shoot \'Em Up', titleStyle);
-      //this.titleText.anchor.setTo(0.5, 0.5);
-      this.scoreText = this.game.add.bitmapText(20, 10, 'fontKubasta','The One Minute Shoot \'Em Up', 48);
+        // Title
+        this.scoreText = this.game.add.bitmapText(20, 10, 'fontKubasta', 'The One Minute Shoot \'Em Up', 48);
 
-    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
-    this.sprite.anchor.setTo(0.5, 0.5);
+        this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
+        this.sprite.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, 0.5);
+        this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+        this.instructionsText.anchor.setTo(0.5, 0.5);
 
-    this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
-  },
-  update: function() {
-    if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
+        this.sprite.angle = -20;
+        this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+    },
+    update: function () {
+        if (this.game.input.activePointer.justPressed()) {
+            this.game.state.start('play');
+        }
     }
-  }
 };
 
 module.exports = Menu;
--- game/states/preload.js
+++ game/states/preload.js
@@ -1,31 +1,31 @@
 'use strict';
 function Preload() {
-  this.asset = null;
-  this.ready = false;
+    this.asset = null;
+    this.ready = false;
 }
 
 Preload.prototype = {
-  preload: function() {
-    this.asset = this.add.sprite(this.width/2,this.height/2, 'preloader');
-    this.asset.anchor.setTo(0.5, 0.5);
+    preload: function () {
+        this.asset = this.add.sprite(this.width / 2, this.height / 2, 'preloader');
+        this.asset.anchor.setTo(0.5, 0.5);
 
-    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
-    this.load.setPreloadSprite(this.asset);
-    this.load.bitmapFont('fontKubasta', 'assets/fonts/kubasta/font.png', 'assets/fonts/kubasta/font.fnt');
-    this.load.spritesheet('hero', 'assets/hero-hitted-and-damaged.png',40,480);
+        this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+        this.load.setPreloadSprite(this.asset);
+        this.load.bitmapFont('fontKubasta', 'assets/fonts/kubasta/font.png', 'assets/fonts/kubasta/font.fnt');
+        this.load.spritesheet('hero', 'assets/hero-hitted-and-damaged.png', 40, 480);
 
-  },
-  create: function() {
-    this.asset.cropEnabled = false;
-  },
-  update: function() {
-    if(!!this.ready) {
-      this.game.state.start('menu');
+    },
+    create: function () {
+        this.asset.cropEnabled = false;
+    },
+    update: function () {
+        if (!!this.ready) {
+            this.game.state.start('menu');
+        }
+    },
+    onLoadComplete: function () {
+        this.ready = true;
     }
-  },
-  onLoadComplete: function() {
-    this.ready = true;
-  }
 };
 
 module.exports = Preload;
