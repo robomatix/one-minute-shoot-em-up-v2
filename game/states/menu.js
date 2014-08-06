87e287d6affbc38a90d57d080d6869cad2278475
robomatix <robomatix@robomatix.net>
2014-08-06 15:24:19 +0200

    menu : yeoman sprite removed

--- game/states/menu.js
+++ game/states/menu.js
@@ -18,7 +18,7 @@ Menu.prototype = {
         var pitchTextString3 = "save";
         var pitchTextString4 = "the";
         var pitchTextString5 = "world !!!";
-        this.pitchText1 = this.game.add.bitmapText(20, 58, 'fontSilkscreen', pitchTextString1, 56);
+        this.pitchText1 = this.game.add.bitmapText(30, 58, 'fontSilkscreen', pitchTextString1, 56);
         this.pitchText2 = this.game.add.bitmapText(230, 110, 'fontSilkscreen', pitchTextString2, 18);
         this.pitchText3 = this.game.add.bitmapText(155, 130, 'fontSilkscreen', pitchTextString3, 56);
         this.pitchText4 = this.game.add.bitmapText(220, 180, 'fontSilkscreen', pitchTextString4, 18);
@@ -29,16 +29,9 @@ Menu.prototype = {
         this.hero.anchor.setTo(0.5, 0.5);
         this.game.add.tween(this.hero).to({x:470}, 5000, Phaser.Easing.Linear.NONE, true, 0, 10000, true);
 
-
-
-        this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
-        this.sprite.anchor.setTo(0.5, 0.5);
-
         this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
         this.instructionsText.anchor.setTo(0.5, 0.5);
 
-        this.sprite.angle = -20;
-        this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
     },
     update: function () {
         if (this.game.input.activePointer.justPressed()) {
