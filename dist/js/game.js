1b16ab9d13de9440eddc620a6dce38a31287d731
robomatix <robomatix@robomatix.net>
2014-08-11 11:22:51 +0200

    Play : Starfield below the hero and better starfiel look and feel

--- dist/js/game.js
+++ dist/js/game.js
@@ -127,6 +127,22 @@ Play.prototype = {
     create: function () {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        /* Initialise emitters
+         **********************************************/
+
+        // Add a starfield to the background of the game
+        var startEmitter = this.game.add.emitter(this.game.world.centerX, 0, 777);
+        startEmitter.width = this.game.world.width;
+        startEmitter.makeParticles('pixel');
+        startEmitter.setYSpeed(18, 88);
+        startEmitter.setXSpeed(0, 0);
+        startEmitter.minParticleScale = 0.1;
+        startEmitter.maxParticleScale = 0.5;
+        startEmitter.minRotation = 0;
+        startEmitter.maxRotation = 0;
+        startEmitter.gravity = 0;
+        startEmitter.start(false, 20000, 100, 0);
+
         /* Add all the sprites/groups to the game
          *********************************************/
 
@@ -135,23 +151,6 @@ Play.prototype = {
         this.game.physics.arcade.enable(this.player);
         this.player.anchor.setTo(0.5, 0.5);
         this.player.body.collideWorldBounds = true;
-
-
-        /* Initialise emitters
-         **********************************************/
-
-        // Add a starfield to the background of the game
-        var startEmitter = this.game.add.emitter(this.game.world.centerX, 0, 200);
-        startEmitter.width = this.game.world.width;
-        startEmitter.makeParticles('pixel');
-        startEmitter.setYSpeed(18, 88);
-        startEmitter.setXSpeed(0, 0);
-        startEmitter.minParticleScale = 0.1;
-        startEmitter.maxParticleScale = 0.7;
-        startEmitter.minRotation = 0;
-        startEmitter.maxRotation = 0;
-        startEmitter.gravity = 0;
-        startEmitter.start(false, 20000, 100, 0);
 
 
     },
